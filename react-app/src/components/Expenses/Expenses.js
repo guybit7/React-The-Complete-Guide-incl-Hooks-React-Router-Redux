f71766f53f74eb1c762c4ae0ccf246523b469ff0
XGQ376 <guybiton7p@gmail.com>
2022-07-18 16:44:11 +0300

    Rendering Lists of Data

--- react-app/src/components/Expenses/Expenses.js
+++ react-app/src/components/Expenses/Expenses.js
@@ -16,30 +16,15 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpenseFilter selected={filteredYear} onFilterChangeHandler={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-          id={props.items[0].id}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-          id={props.items[0].id}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-          id={props.items[0].id}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-          id={props.items[0].id}
-        />
+        {
+          props.items.map(expense =>
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />)}
+
       </Card>
     </div>
   );
--- react-app/src/components/NewExpense/NewExpense.js
+++ react-app/src/components/NewExpense/NewExpense.js
@@ -10,7 +10,7 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         }
         console.log(expenseData);
-        props.onAddExpenseHanlder(expenseData)
+        props.onAddExpense(expenseData)
     }
 
     return (<div className='new-expense'>
