62b8e2b420743934d8255d5dc189fd7316799e0b
XGQ376 <guybiton7p@gmail.com>
2022-07-18 07:59:14 +0300

    add ExpenseForm component + working with multiple states

--- react-app/src/components/Expenses/ExpenseItem.js
+++ react-app/src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
-
 import Card from "../UI/Card";
 import ExpenseDate from './ExpenseDate';
 import MsButton from '../UI/MsButton';
@@ -7,18 +7,26 @@ import MsButton from '../UI/MsButton';
 
 const ExpenseItem = (props) => {
 
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        console.log('updated!');
+        setTitle('updated!')
+    }
+
     return (
         <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>
-                    {props.title}
+                    {title}
                 </h2>
                 <div className="expense-item__price">
                     {props.amount}
                 </div>
             </div>
-            <MsButton text={'Change Title'} id={'expense-item-change-title-btn-'+props.id}/>
+            <MsButton text={'Change Title'} id={'expense-item-change-title-btn-' + props.id} clickHandler={() => clickHandler()} />
+            {/* <button onClick={clickHandler}> Change Title </button> */}
         </Card>
     );
 }
--- react-app/src/components/UI/MsButton.js
+++ react-app/src/components/UI/MsButton.js
@@ -1,10 +1,12 @@
 import Button from '@mui/material/Button';
+import './MsButton.css';
 
 const MsButton = (props) => {
-    return (
-        <Button id={props.id}>{props.text}</Button>
-    );
+    const classes = 'ms-button ' + props.className;
 
+    return (
+        <Button className={classes} id={props.id} onClick={props.clickHandler}>{props.text}</Button>
+    );
 }
 export default MsButton;
 
